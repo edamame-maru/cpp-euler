34ec6850b873ad171569ff832301e9ffc7a1895c
edamame-maru <so1maru.ueda@gmail.com>
2024-03-14 21:29:45 +0000

    First commit

---
+++ euler2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int prev = 1;
+    int current = 1;
+    
+    while (current <= 4000000)
+    {
+        int next = prev + current;
+        prev = current;
+        current = next;
+        
+        if (current % 2 == 0)
+        {
+            sum += current;
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
---
+++ euler3.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(long long n)
+{
+	if (n == 0 || n == 1)
+	{
+		return false;
+	}
+
+	for (long long i = 2; i * i <= n; ++i)
+	{
+		if (n % i == 0)
+		{
+			return false;
+		}
+	}
+
+	return true;
+
+}
+
+int main()
+{
+	long long number = 600851475143;
+	long long currentMax = -1;
+	for (long long i = 2; i * i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			if (isPrime(i) == true)
+			{
+				if(i > currentMax)
+				{
+					currentMax = i;
+				}
+			}
+		}
+	}
+	
+	cout << currentMax;
+	return 0;
+}
